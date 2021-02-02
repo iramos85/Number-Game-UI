25c010d44d13a9192b1c740ca71606f912b40bf8
Ian Ramos <juventuddisponible@gmail.com>
2021-02-02 15:04:44 -0600

    finished game with scenes and added game logic

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int maxGuess;
+    [SerializeField] int minGuess;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        //guess = (maxGuess + minGuess) / 2;
+        NextGuess();
+    }
+    public void OnPressHigher()
+    {
+        minGuess = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        maxGuess = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(minGuess, maxGuess);
+        guessText.text = guess.ToString();
+    }
+}
